d108e6f05d7619fcdaff1ed052f9d166af7612a9
MrRKC <apamrrkc@gmail.com>
2023-12-30 12:57:59 +0700

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,8 @@ const Header = () => {
     return (
         <div className='header'>
             <h2>ShiRyu</h2>
-            <h1>Income / Expense Tracker</h1>         
+            <h1>Income / Expense Tracker</h1> 
+            <h3></h3>     
         </div>
     )
 }
